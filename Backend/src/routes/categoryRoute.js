621ec5f844930baeebb5927da4d21b5e98e79eee
Anand Rathore <anand.s.r9829@gmail.com>
2025-11-20 10:43:25 +0530

    Remove Backend folder from Git tracking

--- Backend/src/routes/categoryRoute.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router()
-
-const CategoryController = require("../controllers/categoryControllers")
-
-router.get('/categories/:category', CategoryController.getCategory)
-
-module.exports = router
--- Backend/src/routes/itemRoute.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const router = express.Router();
-const ItemControllers = require("../controllers/itemControllers")
-
-router.get("/all-items", ItemControllers.getAllItems);
-router.get("/items", ItemControllers.getSearchedItems);
-router.get("/items/:id", ItemControllers.getSingleItem);
-
-module.exports = router
