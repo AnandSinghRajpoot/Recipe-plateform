51efb23e3e9b8199000290b69b9c1555c9937f48
unknown <mustafaraza03898@gmail.com>
2026-01-27 11:37:48 +0530

    final commit.

--- Backend/src/main/java/com/recipeplatform/dto/ErrorResponse.java
+++ Backend/src/main/java/com/recipeplatform/dto/ErrorResponse.java
@@ -1,21 +1,18 @@
 package com.recipeplatform.dto;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.time.LocalDateTime;
+import java.util.Map;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-@Builder
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class ErrorResponse {
-
-    private LocalDateTime timestamp;
-    private int status;
-    private String error;
     private String message;
-    private String path;
+    private Map<String,String > errors;
+    private int status;
 }
