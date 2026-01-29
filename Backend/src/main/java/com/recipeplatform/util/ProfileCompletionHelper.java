5f87b0603c288762d80248442e990c8daaa4e515
unknown <mustafaraza03898@gmail.com>
2026-01-29 12:34:37 +0530

    Refactor: Update ProfileCompletionHelper logic

--- Backend/src/main/java/com/recipeplatform/util/ProfileCompletionHelper.java
+++ Backend/src/main/java/com/recipeplatform/util/ProfileCompletionHelper.java
@@ -4,25 +4,13 @@ import com.recipeplatform.domain.User;
 
 public class ProfileCompletionHelper {
 
-    //initialize at first time
+    // initialize at first time
     public static void initialize(User user) {
         user.setIsProfileCompleted(false);
-        user.setIsReminderDismissed(false);
     }
 
-    //at profile completion
+    // at profile completion
     public static void completeProfile(User user) {
         user.setIsProfileCompleted(true);
-        user.setShowReminder(false);
-    }
-
-    //at reminder dismissed
-    public static void reminderDismissed(User user) {
-        user.setIsReminderDismissed(true);
-        user.setShowReminder(false);
-    }
-
-    public static boolean shouldShowReminder(User user) {
-        return !user.getIsProfileCompleted() && !user.getIsReminderDismissed();
     }
 }
