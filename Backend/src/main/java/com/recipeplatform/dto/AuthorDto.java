66a64ed043a156e049bfa2705f334bac1352fcaf
unknown <mustafaraza03898@gmail.com>
2026-01-29 12:39:30 +0530

    Refactor: Update AuthorDto

--- Backend/src/main/java/com/recipeplatform/dto/AuthorDto.java
+++ Backend/src/main/java/com/recipeplatform/dto/AuthorDto.java
@@ -1,4 +1,14 @@
 package com.recipeplatform.dto;
 
+import com.recipeplatform.domain.enums.UserRole;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class AuthorDto {
+    private Long id;
+    private UserRole role;
 }
