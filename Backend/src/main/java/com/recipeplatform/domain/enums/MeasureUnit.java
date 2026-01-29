3792639d6376bc8000a9188ad992b93fab31eda0
unknown <mustafaraza03898@gmail.com>
2026-01-29 12:39:24 +0530

    Refactor: Update MeasureUnit enum

--- Backend/src/main/java/com/recipeplatform/domain/enums/MeasureUnit.java
+++ Backend/src/main/java/com/recipeplatform/domain/enums/MeasureUnit.java
@@ -1,4 +1,28 @@
 package com.recipeplatform.domain.enums;
 
-public enum MeasureUnits {
+import com.fasterxml.jackson.annotation.JsonCreator;
+
+public enum MeasureUnit {
+        GRAM,
+        KILOGRAM,
+        LITER,
+        MILLILITER,
+        CUP,
+        TABLESPOON,
+        TEASPOON,
+        PIECE;
+
+
+        @JsonCreator
+        public static MeasureUnit fromValue(String unit){
+                for (MeasureUnit measureUnit:MeasureUnit.values()){
+                        if (measureUnit.name().equalsIgnoreCase(unit)) {
+                                return measureUnit;
+                        }
+                }
+                throw new IllegalArgumentException(
+                        "Invalid diet type: " + unit + ". Allowed values: GRAM,KILOGRAM,LITER,MILLILITER,CUP,TABLESPOON,TEASPOON,PIECE."
+                );
+        }
+
 }
