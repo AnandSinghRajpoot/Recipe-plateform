cbc58434115fbe79ac91314f14c20bb366b02581
unknown <mustafaraza03898@gmail.com>
2026-01-29 12:39:19 +0530

    Refactor: Update Difficulty enum with JsonCreator

--- Backend/src/main/java/com/recipeplatform/domain/enums/Difficulty.java
+++ Backend/src/main/java/com/recipeplatform/domain/enums/Difficulty.java
@@ -1,5 +1,19 @@
 package com.recipeplatform.domain.enums;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+
 public enum Difficulty {
-    EASY,MEDIUM,HARD
+    EASY,MEDIUM,HARD;
+
+    @JsonCreator
+    public static Difficulty fromValue(String difficulty){
+        for (Difficulty type:Difficulty.values()){
+            if (type.name().equalsIgnoreCase(difficulty)) {
+                return type;
+            }
+        }
+        throw new IllegalArgumentException(
+                "Invalid diet type: " + difficulty + ". Allowed values: EASY,MEDIUM,HARD"
+        );
+    }
 }
