9bca05a83aafe961a1e68aecacbcda7249014128
unknown <mustafaraza03898@gmail.com>
2026-01-31 11:37:37 +0530

    feat(backend): update ApiResponse to support generic data payload

--- Backend/src/main/java/com/recipeplatform/dto/ApiResponse.java
+++ Backend/src/main/java/com/recipeplatform/dto/ApiResponse.java
@@ -7,6 +7,8 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class ApiResponse {
+public class ApiResponse<T>{
     private String message;
+    private T data;
+    private int status;
 }
