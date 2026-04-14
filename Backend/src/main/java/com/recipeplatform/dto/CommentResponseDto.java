7646ca31e8634ab757b1b971c18535453a8798b8
unknown <mustafaraza03898@gmail.com>
2026-04-14 23:55:03 +0530

    Add CommentResponseDto for comment responses

---
+++ Backend/src/main/java/com/recipeplatform/dto/CommentResponseDto.java
@@ -0,0 +1,19 @@
+package com.recipeplatform.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class CommentResponseDto {
+    private Long id;
+    private String content;
+    private AuthorDto author;
+    private LocalDateTime createdAt;
+}
